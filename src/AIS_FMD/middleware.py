8e8fd1f29819d755c6bdafeb2759ce8661507e14
udaya2308 <udaya14apr2000@gmail.com>
2024-03-01 14:08:00 -0500

    add middleware for authentication

---
+++ src/AIS_FMD/middleware.py
@@ -0,0 +1,19 @@
+from django.shortcuts import redirect
+
+class AdminLoginRequiredMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        if not request.user.is_authenticated:
+            if request.path.startswith("/login") or request.path.startswith("/signup"):
+                pass
+            else:
+                return redirect("/login")
+        elif request.path.startswith("/admin") and not request.user.has_perm(
+                "admin.access_admin"
+        ):
+            return redirect("/")
+
+        return self.get_response(request)
--- src/AIS_FMD/settings.py
+++ src/AIS_FMD/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "AIS_FMD.middleware.AdminLoginRequiredMiddleware",
 ]
 
 ROOT_URLCONF = "AIS_FMD.urls"
