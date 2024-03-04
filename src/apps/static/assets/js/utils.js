59fb4ab183f8f9c8c8403a40c885d3a032d03c0f
udaya2308 <udaya14apr2000@gmail.com>
2024-03-04 08:50:54 -0500

    fix date format

--- src/apps/static/assets/js/utils.js
+++ src/apps/static/assets/js/utils.js
@@ -8,6 +8,15 @@ async function fetchApiJsonData(url) {
     }
 }
 
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
+
 function calculateDaysLeft(dateStr) {
     const date = new Date(dateStr);
     const today = new Date();
@@ -75,7 +84,7 @@ function calculateDailyIncomeExpense(transactions) {
     const dailyData = {};
 
     transactions.forEach(transaction => {
-        const date = new Date(transaction.Date).toLocaleDateString();
+        const date = formatDate(transaction.Date);
         const amount = parseFloat(transaction.Amount);
 
         if (!(date in dailyData)) {
@@ -89,7 +98,9 @@ function calculateDailyIncomeExpense(transactions) {
         }
     });
 
-    return Object.values(dailyData);
+    return Object.keys(dailyData)
+    .sort((a, b) => new Date(a) - new Date(b))
+    .map(key => dailyData[key]);;
 }
 
 function calculateIncomeExpenseByPurpose(transactions) {
